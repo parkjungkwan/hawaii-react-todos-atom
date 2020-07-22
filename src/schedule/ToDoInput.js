86cec690c900c59bffef76890ef20a56b5c15d35
parksrazor <pakjkwan@gmail.com>
2020-07-22 13:38:57 +0900

    first commit

---
+++ src/schedule/ToDoInput.js
@@ -0,0 +1,50 @@
+import React,{useState} from "react";
+import {useDispatch} from 'react-redux'
+import {addTodoAction} from './store/schedule.action'
+import uuid from 'uuid/v4'
+
+const ToDOInput =() => {
+    const [todo, setTodo] = useState("")
+    const dispatch = useDispatch()
+    const addToDo = todo => dispatch(addTodoAction(todo))
+
+    const handleChange = e => {
+        e.preventDefault()
+        setTodo(e.target.value)
+    }
+    const submitForm = e => {
+        e.preventDefault()
+        const newTodo = {
+            id: uuid(),
+            name: todo,
+            complete: false
+        }
+        addToDo(newTodo)
+        setTodo("")
+    }
+
+
+    return <>
+        <form onSubmit={submitForm} method="POST">
+            <div className="row mt-3">
+                <div className="form-group col-sm-8">
+                    <input
+                        type="text"
+                        placeholder="Add a todo"
+                        name="todo"
+                        className="form-control"
+                        value={todo}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="col-sm-4">
+                    <button type="submit" className="btn btn-primary btn-md">
+                        Add Todo
+                    </button>
+                </div>
+            </div>
+        </form>
+    </>
+}
+
+export default ToDOInput
---
+++ src/schedule/ToDoList.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {toggleTodoAction, deleteTodoAction} from './store/schedule.action'
+
+
+const ToDoList = () => {
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+    const toggleTodo = todoId => dispatch(toggleTodoAction(todoId))
+    // const deleteTodo = ?
+    return <>
+        <div>
+            {todos && todos.length === 0 && (
+                <p className="alert alert-info">No Todos at the moment</p>
+            )}
+            {todos &&
+            todos.map(todo => (
+                <div key={todo.id} className="row mb-1">
+                    <div className="col-sm-2">
+                        <input
+                            type="checkbox"
+                            checked={todo.complete}
+                            onChange={toggleTodo.bind(null, todo.id)}
+                        />
+                    </div>
+                    <div className="col-sm-8">
+                        <span>{todo.name}</span>
+                    </div>
+                    <div className="col-sm-2">
+                        <button
+                            className="btn btn-danger"
+                            onClick={deleteTodo.bind(null, todo.id)}
+                        >
+                            X
+                        </button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </>
+}
+
+export default ToDoList
